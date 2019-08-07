7931fd4284ae513685ca8a667d53e668bee5402d
ethanblumenthal <ethan.blumenthal@gmail.com>
2019-08-07 16:43:14 -0500

    abstracted model pass through methods into their own class

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'Ethan', age: 26 });
-
-user.on('save', () => {
-  console.log('save');
-});
-
-user.save();
+const user = User.buildUser({ id: 100 });
