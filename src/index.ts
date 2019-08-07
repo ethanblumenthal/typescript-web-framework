b36d7deca7d0a7a394444e60a42f2f60e07c5b7e
ethanblumenthal <ethan.blumenthal@gmail.com>
2019-08-07 14:31:51 -0500

    created fetch and save methods on user class to persist data to json server

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
 import { User } from './models/User';
 
 const user = new User({ name: 'Ethan', age: 26 });
-
-user.on('change', () => {
-  console.log('change');
-});
-user.trigger('change');
+user.save();
